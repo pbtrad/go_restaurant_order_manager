3c551adfb5b37864e30cac601c3c042e2283eef9
paul <paulbrowne2@gmail.com>
2022-02-19 21:49:40 +0000

    Created .gitignore and order folders

--- server/main.go
+++ server/main.go
@@ -22,19 +22,13 @@ func main() {
 
 	router.Use(cors.Default())
 
-	// these are the endpoints
-	//C
 	router.POST("/order/create", routes.AddOrder)
-	//R
 	router.GET("/waiter/:waiter", routes.GetOrdersByWaiter)
 	router.GET("/orders", routes.GetOrders)
 	router.GET("/order/:id/", routes.GetOrderById)
-	//U
 	router.PUT("/waiter/update/:id", routes.UpdateWaiter)
 	router.PUT("/order/update/:id", routes.UpdateOrder)
-	//D
 	router.DELETE("/order/delete/:id", routes.DeleteOrder)
 
-	//this runs the server and allows it to listen to requests.
 	router.Run(":" + port)
 }
